840773ea292d29d6bc2a10906e7ab82f071e9760
Nubb <giloe.dev@gmail.com>
2022-12-14 17:33:48 -0700

    fixed

--- index.ts
+++ index.ts
@@ -1,32 +1,4 @@
-<<<<<<< Updated upstream
-import * as fs from 'fs'
 
-const input = fs.readFileSync('input.txt').toString().split('\n')
-const inputArray: Array<Array<number>> = []
-const finalArray: Array<number> = []
-let sol:number = 0
-
-let temp1: Array<number> = []
-let temp2: number
-input.forEach((string:string) => {
-    if (string !== '') {
-        temp1.push(parseInt(string))
-        return;
-    }
-    inputArray.push(temp1)
-    temp1 = []
-})
-inputArray.forEach(cal => {
-    temp2 = cal.reduce((partialSum, a) => partialSum + a, 0)
-    finalArray.push(temp2)
-})
-
-finalArray.sort((a, b) => b - a).slice(0, 3).forEach((num: number) => {
-    sol += num
-})
-
-console.log(sol)
-=======
 const inputFile = await Deno.readTextFile("input.txt").then(file => {
     return file.split('\n');
 });
@@ -100,4 +72,3 @@ tempArray.forEach(stringArray => {
 })
 
 console.log(inputNumb2.reduce((a, b) => a + b, 0))
->>>>>>> Stashed changes
